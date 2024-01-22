5459a5d593689c8bbd032aa164f6d8b5a8ce4155
Utilizzatore <Utilizzatore@DESKTOP-4DDKKF3>
2024-01-22 20:35:01 +0100

    Aggiungere i file di progetto.

---
+++ PersonaEtaClasse/Persona.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonaEtaClasse
+{
+    internal class Persona
+    {
+        public DateTime DataN { get; set; }
+
+        public int Calcoloe()
+        {
+            Persona p = new Persona();
+            int mn = DataN.Month;
+            int gn = DataN.Day;
+            int an = DataN.Year;
+
+            int anno = DateTime.Now.Year;
+            int mese = DateTime.Now.Month;
+            int gg = DateTime.Now.Day;
+
+            if(mn > mese)
+            {
+                return anno - an - 1;
+            }
+            else if(mn < mese)
+            {
+                return anno - an;
+            }
+            else if(gn > gg)
+            {
+                return anno - an - 1;
+            }else
+            {
+                return anno - an;
+            }
+        }
+
+    }
+}
---
+++ PersonaEtaClasse/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonaEtaClasse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona p = new Persona();
+            p.DataN = DateTime.Parse(Console.ReadLine());
+
+            Console.WriteLine("La tua età è: {0}", p.Calcoloe());
+            Console.ReadLine();
+        }
+    }
+}
